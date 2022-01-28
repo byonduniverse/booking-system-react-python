d17c3299f559bc3a5d61e52fc9aa197d350c0182
byonduniverse <masterjames318@gmail.com>
2022-01-28 16:50:47 +0100

    changes

--- src/manager.py
+++ src/manager.py
@@ -16,12 +16,20 @@ class Slot:
     def __init__(self) -> None:
         self.bookings: List[Booker] = []
     
+
+    def lock(self) -> None:
+        pass
+    
     
     def is_slot_free(self, index: int, time_start: datetime.datetime, time_end: datetime.datetime) -> bool:
         for booking in self.bookings:
             if booking.time_start < time_end and booking.time_end > time_start:
                 return False
         return True
+    
+
+    def find_free_hours(self, duration: datetime.timedelta) -> List[Tuple[datetime.datetime, datetime.datetime]]:
+        pass
 
 
 
